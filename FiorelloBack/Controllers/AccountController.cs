edd1003d831a5de5d85343bd2bc1181fc759dcb3
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-01-23 20:10:01 +0400

    Created

---
+++ FiorelloBack/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using FiorelloBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorelloBack.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterVM register)
+        {
+            if(!ModelState.IsValid) return View();
+            return View(register);
+        }
+    }
+}
